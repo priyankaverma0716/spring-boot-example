ac13196321be2fcc61461a4f506fc468523613a1
sourabhverma <priyanka.verma0716@gmail.com>
2022-01-23 19:08:45 -0600

    New files added for spring boot application

--- src/main/java/com/infosys/example/springbootexample/SpringBootExampleApplication.java
+++ src/main/java/com/infosys/example/springbootexample/SpringBootExampleApplication.java
@@ -1,4 +1,4 @@
-package com.infosys.example.springbooteapmle;
+package com.infosys.example.springbootexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/infosys/example/springbootexample/SpringBootExampleApplicationTests.java
+++ src/test/java/com/infosys/example/springbootexample/SpringBootExampleApplicationTests.java
@@ -1,4 +1,4 @@
-package com.infosys.example.springbooteapmle;
+package com.infosys.example.springbootexample;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
